e3d2c6579a0cba898db25be74e6f9478f3270552
Karam Talwandi <k.talwandi@hotmail.com>
2018-06-11 19:58:30 -0400

    Selection screen works

---
+++ WatFitProt/CustomCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CustomCollectionViewCell.swift
+//  WatFitProt
+//
+//  Created by Karam Puneet Talwandi on 2018-06-11.
+//  Copyright © 2018 GTN. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var cellImage: UIImageView!
+    
+    @IBOutlet weak var cellLabel: UILabel!
+    
+    @IBOutlet weak var cellButton: UIButton!
+
+}
---
+++ WatFitProt/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  WatFitProt
+//
+//  Created by Karam Puneet Talwandi on 2018-06-11.
+//  Copyright © 2018 GTN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    let excercises = ["Bench Press","OverHead Press", "Squat", "Barbell Rows", "Lat Pulldowns", "Peck Deck"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+    
+    
+    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int{
+        return excercises.count
+    }
+    
+    
+    // The cell that is returned must be retrieved from a call to -dequeueReusableCellWithReuseIdentifier:forIndexPath:
+    
+    public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell{
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "customCell", for: indexPath) as! CustomCollectionViewCell
+        cell.cellImage.image = UIImage(named: excercises[indexPath.row])
+        cell.cellLabel.text = excercises[indexPath.row]
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("You selected cell #\(indexPath.item)!")
+        
+    }
+    
+   
+
+    
+}
+
